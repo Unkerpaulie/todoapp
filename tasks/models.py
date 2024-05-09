dd17244c90392e287e8a8aa68461bf083a9c55f0
Paul Pedro <unkerpaulie@gmail.com>
2024-05-09 07:36:01 -0400

    Created Task model and applied migrations

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    due_date = models.DateField(null=True, blank=True)
+    due_time = models.TimeField(null=True, blank=True)
+    done = models.BooleanField(default=False)
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
