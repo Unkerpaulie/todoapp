aa37072a9c859f6a8344e59fef0b7d7ca70cc01f
Paul Pedro <unkerpaulie@gmail.com>
2024-05-09 06:58:42 -0400

    added pages, views and url patterns for default page layouts

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,4 +5,9 @@ app_name = "tasks"
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('new/', views.new, name="new"),
+    path('done/', views.done, name="done"),
+    path('undone/', views.undone, name="undone"),
+    path('detail/', views.detail, name="detail"),
+    path('delete/', views.delete, name="delete"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 
 def home(req):
     return render(req, "tasks/home.html")
+
+def new(req):
+    return render(req, "tasks/new.html")
+
+def undone(req):
+    return render(req, "tasks/undone.html")
+
+def done(req):
+    return render(req, "tasks/done.html")
+
+def detail(req):
+    return render(req, "tasks/detail.html")
+
+def delete(req):
+    return render(req, "tasks/delete.html")
